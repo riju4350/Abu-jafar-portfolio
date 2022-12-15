e600a08620ce5fc7285a408289fc321fda917f01
riju4350 <riju4350@gmail.com>
2022-12-15 23:23:06 +0600

    Social buttons and Heading text  added

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Data } from "./Data";
+import "./Home.css";
+import { Social } from "./Social";
+
+export const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home_container container grid">
+        <div className="home_content grid">
+          <Social />
+          <div className="home_img"></div>
+          <Data />
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Social = () => {
+  return (
+    <div className="home_social">
+      <a
+        href="https://www.facebook.com/"
+        className="home_social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-facebook-f"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/"
+        className="home_social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.github.com/"
+        className="home_social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
