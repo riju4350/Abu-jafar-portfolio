8af6382734ac0392b736e0bf69ba3dbd2f553ab5
riju4350 <riju4350@gmail.com>
2022-12-15 18:28:14 +0600

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <div className="">
-      <h1>Abu jafar</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
