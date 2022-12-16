1adfb650198931b6ca3827320552c173c1aaa116
riju4350 <riju4350@gmail.com>
2022-12-17 00:26:24 +0600

    Skills section added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./Components/About/About";
 import Header from "./Components/Header/Header";
 import { Home } from "./Components/Home/Home";
+import Skills from "./Components/Skills/Skills";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Skills />
       </main>
     </>
   );
---
+++ src/Components/Skills/Backend.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills_content">
+      <h3 className="skills_title">Backend Developer</h3>
+
+      <div className="skills_box">
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Node.js</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Python</h3>
+              <span className="skills_level">Basic</span>
+            </div>
+          </div>
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Netlify</h3>
+              <span className="skills_level">Basic</span>
+            </div>
+          </div>
+        </div>
+        <div className="skills_group">
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">MongoDB</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Express.js</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills_data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills_name">Firebase</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
